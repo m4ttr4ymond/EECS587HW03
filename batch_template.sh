362bc70766423d8c7745a94a2823f540e7c415f0
m4ttr4ymond <m4tt.r4ymond@gmail.com>
2020-10-23 10:09:01 -0400

    init

---
+++ batch_template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#============================================================================
+#Name        : batch_template.sh
+#Author      : Matt Raymond
+#Class       : EECS 587, Spring 2020
+#Description : Template for HW2
+#============================================================================
+
+#SBATCH --job-name=02_p<p_num>_m<m_num>_n<n_num>
+#SBATCH --mail-type=NONE
+#SBATCH --nodes=<p_num>
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=1g 
+#SBATCH --time=00:05:00
+#SBATCH --account=eecs587f20_class
+#SBATCH --partition=standard
+
+mpirun -np <p_num> ./02 <m_num> <n_num> >> <file_name>
---
+++ startup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#============================================================================
+#Name        : startup.sh
+#Author      : Matt Raymond
+#Class       : EECS 587, Spring 2020
+#Description : Handles compilation and slurm job submission for HW2
+#============================================================================
+
+# Load in modules
+module load gcc
+module load openmpi
+
+# Compiler the program with the given compiler optimization level
+mpic++ $1 02.cpp -o 02
+
+# Remove excess slurm files
+if compgen -G "./slurm-*" > /dev/null; then
+	rm ./slurm-*
+fi
+
+# Make the output directory if it doesn't exist
+if ! compgen -G "./output/" > /dev/null; then
+	mkdir ./output
+fi
+
+EXPORT_FILENAME=/home/mattrmd/programs/EECS587HW02/output/result.out
+
+# Remove the output file
+if compgen -G $EXPORT_FILENAME > /dev/null; then
+	rm $EXPORT_FILENAME
+fi
+
+# Cycle through matrix sizes
+for i in 2000,500 1000,4000; do IFS=","; set -- $i;
+    # Cycle through processor numbers
+   for p in 1 4 16 36
+    do
+        FILENAME=./$1_$2_$p.sh
+        # Copy the batch file template
+        cp ./batch_template.sh $FILENAME
+
+        # Replace the template terms
+        sed -i -e "s/<p_num>/$p/" $FILENAME
+        sed -i -e "s/<m_num>/$1/" $FILENAME
+        sed -i -e "s/<n_num>/$2/" $FILENAME
+        sed -i -e "s|<file_name>|$EXPORT_FILENAME|" $FILENAME
+
+        # Send the file to slurm
+        sbatch $FILENAME
+
+        # Remove the batch file
+        rm $FILENAME
+    done
+done
+
+# Print all of the processes that are in the queue
+squeue -u mattrmd
